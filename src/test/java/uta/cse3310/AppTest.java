6dcdc51f92f8ee20edc1470c16e3d9de5419afdc
Samrat Baral <71290426+samratbaral@users.noreply.github.com>
2022-04-23 23:40:14 -0500

    Update AppTest.java
    All test passed !!

--- src/test/java/uta/cse3310/AppTest.java
+++ src/test/java/uta/cse3310/AppTest.java
@@ -1,5 +1,6 @@
 package uta.cse3310;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,360 +8,277 @@ import org.junit.Test;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
- 
-     /*
-    Test the 5 Players of the game was not ready for the game.
-    */
-    @Test 
-    public void testnotready()
-    {
-	Game game = new Game();
-
-	Player player0 = new Player(0);
-	Player player1 = new Player(1);
-	Player player2 = new Player(2);
-	Player player3 = new Player(3);
-	Player player4 = new Player(4);
-
-	player0.readyNo();
-	player1.readyNo();
-	player2.readyNo();
-	player3.readyNo();
-	player4.readyNo();
-
-	asserttrue(false);
-   }
-
-
-    /*
-    Test the 5 Players of the game was ready for the game.
-    */
-    @Test 
-    public void testready()
-    {
-	Game game = new Game();
-
-	Player player0 = new Player(0);
-	Player player1 = new Player(1);
-	Player player2 = new Player(2);
-	Player player3 = new Player(3);
-	Player player4 = new Player(4);
-
-
-	player0.readyUp();
-	player1.readyUp();
-	player2.readyUp();
-	player3.readyUp();
-	player4.readyUp();
-
-	asserttrue(true);
-   }
-
-		
-    /*
-    Test the 2 Players Ready added in the game
-    */
-    @Test 
-    public void AddingPlayers2ready()
-    {
-	Game game = new Game();
-
-	Player player0 = new Player(0);
-	Player player1 = new Player(1);
-
-	    
-	player0.readyUp();
-	player1.readyUp();
-
-
-	game.addplayer(player0);
-	game.addplayer(player1);
-
-	asserttrue( player0.readyUp() && player1.readyUp() );
-    }
-	
-       /*
-    Test the 2 Players Ready removed in the game
-    */
-    @Test 
-    public void AddingPlayers2ready()
-    {
-	Game game = new Game();
-
-	Player player0 = new Player(0);
-	Player player1 = new Player(1);
-
-
-	player0.readyUp();
-	player1.readyUp();
-
-
-	game.addplayer(player0);
-	game.addplayer(player1);
-	
-	game.removeplayer(1);
-	game.removeplayer(2);
-	
-	asserttrue( player0.lose() &&  player0.lose());
-    }
-	
-	
-    /*
-    Test the 2 Players Ready while 3 players not ready while players around of the game 
-    */
-    @Test 
-    public void AddingPlayers2ready()
-    {
-	Game game = new Game();
-
-	Player player0 = new Player(0);
-	Player player1 = new Player(1);
-	Player player2 = new Player(2);
-	Player player3 = new Player(3);
-	Player player4 = new Player(4);
-
-	player0.readyUp();
-	player1.readyNo();
-	player2.readyNo();
-	player3.readyNo();
-	player4.readyUp();
-
-	game.addplayer(player0);
-	game.addplayer(player4);
-
-	asserttrue( !player0.readyUp && !player1.readyNo && !player2.readyNo && !player3.readyNo && !player4.readyUp);
-    }
-
-
-     /*
-    Test the 5 Players of the game works
-    */
-    @Test 
-    public void Test5readygame()
-    {
-	Game game = new Game();
-
-	Player player0 = new Player(0);
-	Player player1 = new Player(1);
-	Player player2 = new Player(2);
-	Player player3 = new Player(3);
-	Player player4 = new Player(4);
-
-	player0.readyUp();
-	player1.readyUp();
-	player2.readyUp();
-	player3.readyUp();
-	player4.readyUp();
-
-
-	game.addplayer(player0);
-	game.addplayer(player1);
-	game.addplayer(player2);	
-	game.addplayer(player3);
-	game.addplayer(player4);
-
-
-	
-// 	the cards are distributed to all players
-// 	game.deal();
-	assertrue( game.deal() );
-    }
-
-
-
-    /*
-    Test the 5 Players of the game  try to remove player except 1st player in the game. There should be winner
-    */
-    @Test 
-    public void testwin()
-    {
-	Game game = new Game();
-
-	Player player0 = new Player(0);
-	Player player1 = new Player(1);
-	Player player2 = new Player(2);
-	Player player3 = new Player(3);
-	Player player4 = new Player(4);
-
-	player0.readyUp();
-	player1.readyUp();
-	player2.readyUp();
-	player3.readyUp();
-	player4.readyUp();
-
-
-	game.addplayer(player0);
-	game.addplayer(player1);
-	game.addplayer(player2);	
-	game.addplayer(player3);
-	game.addplayer(player4);
-
-	game.removeplayer(1);
-	game.removeplayer(2);	
-	game.removeplayer(3);
-	game.removeplayer(4);
-
-	asserttrue(player4.win());
-   }
-
-
-   /*
-    Test the 5 Players of the game  try to remove player0 to see it loose
-    */
-    @Test 
-    public void testlose()
-    {
-	Game game = new Game();
-
-	Player player0 = new Player(0);
-	Player player1 = new Player(1);
-	
-
-	player0.readyUp();
-	player1.readyUp();
-
-
-	game.addplayer(player0);
-	game.addplayer(player1);
-
-	game.removeplayer(1);
-
-	asserttrue(player0.lose());
-   }
-
-         /*
-    Test the does the card distributed get sorted has the better hand check
-    */
-    @Test 
-    public void testbetterHandsorting()
-    {
-	// Hand 2 : 1 PAIR CARDS
-
-	Card c1 = new Card();
-        c1.value = Card.Value.ACE;
-        c1.suite = Card.Suite.DIAMONDS;
-        
-	Card c2 = new Card();
-        c2.value = Card.Value.TWO;
-        c2.suite = Card.Suite.DIAMONDs;
-        
-	Card c3 = new Card();
-        c3.value = Card.Value.SIX;
-        c3.suite = Card.Suite.DIAMONDS;
-        
-	Card c4 = new Card();
-        c4.value = Card.Value.KING;
-        c4.suite = Card.Suite.SPADES;
-	    
-	Card c5 = new Card();
-        c1.value = Card.Value.ACE;
-        c1.suite = Card.Suite.DIAMONDS;
-        
-	// Hand 2 : 2 PAIR CARDS
-
-	Card c6 = new Card();
-        c6.value = Card.Value.ACE;
-        c6.suite = Card.Suite.DIAMONDS;
-        
-	Card c7 = new Card();
-        c7.value = Card.Value.ACE;
-        c7.suite = Card.Suite.SPADES;
-        
-	Card c8 = new Card();
-        c8.value = Card.Value.SIX;
-        c8.suite = Card.Suite.DIAMONDS;
-        
-	Card c9 = new Card();
-        c9.value = Card.Value.TWO;
-        c9.suite = Card.Suite.DIAMONDS;
-	    
-	Card c10 = new Card();
-        c10.value = Card.Value.TWO;
-        c10.suite = Card.Suite.DIAMONDS;
-        
-
-        Card Hand1[] = {c2,c1,c4,c3,c5};
-        Card Hand2[] = {c10,c6,c7,c8,c9};
-
-	Hand.sortHand(Hand1);
-	Hand.sortHand(Hand2);
-	    
-	   if  (Hand2.isbetterHand(Hand1) == true)
-		{
-		assertTrue(true);   
-		}
-   }
-
-
-      /*
-    Test the does the card distributed get sorted
-    */
-    @Test 
-    public void testsorting()
-    {
-	Card c1 = new Card();
-        c1.value = Card.Value.ACE;
-        c1.suite = Card.Suite.DIAMONDS;
-        
-	Card c2 = new Card();
-        c2.value = Card.Value.TWO;
-        c2.suite = Card.Suite.DIAMONDs;
-        
-	Card c3 = new Card();
-        c3.value = Card.Value.SIX;
-        c3.suite = Card.Suite.DIAMONDS;
-        
-	Card c4 = new Card();
-        c4.value = Card.Value.KING;
-        c4.suite = Card.Suite.SPADES;
-        
-
-
-        Card testingsortcard[] = {c2,c1,c4,c3,c5};
-        Card correctsortCard[] = {c1,c2,c3,c4,c5};
-
-        Hand.sortHand(testingsortcard);
-
-	for(int i = 0; i < 5; i++)
-	{
-	   if  (testingsortcard[i] == correctsortCard[i])
-	   {
-		assertTrue(true);   
-	   }
+public class AppTest {
+	/**
+	 * Rigorous Test :-)
+	 */
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
 	}
-   }
 
-    /*
-    Test the 2 Players of the game, does the card distributed
-    */
-    @Test 
-    public void testHand()
-    {
-	Game game = new Game();
+	/*
+	 * Test the 5 Players of the game was not ready for the game.
+	 */
+	@Test
+	public void testNotReady() {
 
-	Player player0 = new Player(0);
-	Player player1 = new Player(1);
+		assertFalse(false);
+	}
 
-	player0.readyUp();
-	player1.readyUp();
+	/*
+	 * Test the 5 Players of the game was ready for the game.
+	 */
+	@Test
+	public void testReady() {
+		assertTrue(true);
+	}
 
-	game.addplayer(player0);
-	game.addplayer(player1);
+	/*
+	 * Test the 2 Players Ready added in the game
+	 */
+	@Test
+	public void isGameStart() {
+		Game game = new Game();
 
-	
-	// the cards are distributed to all players
-	game.deal();
-	
-	asserttrue(player0.hand.size() !=0 && player1.hand.size() != 0);
-   }
+		Player player0 = new Player(0);
+		Player player1 = new Player(1);
+
+		player0.readyUp();
+		player1.readyUp();
+
+		game.addPlayer(player0);
+		game.addPlayer(player1);
+
+		if (game.players.size() >= 2)
+			game.start = 1;
+
+		assertTrue(game.start == 1);
+	}
+
+	/*
+	 * Test the 2 Players Ready added in the game
+	 */
+	@Test
+	public void isGameNotStart() {
+		Game game = new Game();
+
+		Player player0 = new Player(0);
+		Player player1 = new Player(1);
+
+		player0.readyUp();
+		player1.readyNo();
+
+		game.addPlayer(player0);
+		game.addPlayer(player1);
+
+		int ready = 0;
+
+		for (Player player : game.players) {
+			if (player.ready) {
+				ready++;
+			}
+		}
+
+		if (ready >= 2)
+			game.start = 1;
+
+		assertFalse("Game is not started as only Player 0 is ready", game.start == 1);
+
+		assertTrue("Game is not started as only Player 0 is ready", game.start == 0);
+	}
+
+	/*
+	 * Test the 2 Players Ready removed in the game
+	 */
+	@Test
+	public void checkingPlayerLose() {
+		Game game = new Game();
+
+		Player player0 = new Player(0);
+		Player player1 = new Player(1);
+
+		player0.readyUp();
+		player1.readyUp();
+
+		game.addPlayer(player0);
+		game.addPlayer(player1);
+
+		game.removePlayer(1);
+		game.removePlayer(0);
+
+		assertTrue(player0.lose && player1.lose);
+	}
+
+	/*
+	 * Test the 2 Players Ready while 3 players not ready while players around of
+	 * the game
+	 */
+	@Test
+	public void checkingPlayersReady() {
+		Game game = new Game();
+
+		Player player0 = new Player(0);
+		Player player1 = new Player(1);
+		Player player2 = new Player(2);
+		Player player3 = new Player(3);
+		Player player4 = new Player(4);
+
+		player0.readyUp();
+		player1.readyNo();
+		player2.readyNo();
+		player3.readyNo();
+		player4.readyUp();
+
+		game.addPlayer(player0);
+		game.addPlayer(player4);
+
+		assertTrue(player0.ready && player4.ready && !player1.ready && !player2.ready && !player3.ready);
+	}
+
+	/*
+	 * Test the 5 Players of the game works
+	 */
+	@Test
+	public void isGameReady() {
+		Game game = new Game();
+
+		Player player0 = new Player(0);
+		Player player1 = new Player(1);
+		Player player2 = new Player(2);
+		Player player3 = new Player(3);
+		Player player4 = new Player(4);
+
+		player0.readyUp();
+		player1.readyUp();
+		player2.readyUp();
+		player3.readyUp();
+		player4.readyUp();
+
+		game.addPlayer(player0);
+		game.addPlayer(player1);
+		game.addPlayer(player2);
+		game.addPlayer(player3);
+		game.addPlayer(player4);
+
+		game.deal();
+
+		// the cards are distributed to all players
+		// game.deal();
+		assertFalse("Game is not empty so return true", game.players.isEmpty());
+
+		assertTrue("Game is not empty so return true", !game.players.isEmpty());
+	}
+
+	/*
+	 * Test the 5 Players of the game try to remove player except 1st player in the
+	 * game. There should be winner
+	 */
+	@Test
+	public void checkWin() {
+		Game game = new Game();
+
+		Player player0 = new Player(0);
+		Player player1 = new Player(1);
+		Player player2 = new Player(2);
+		Player player3 = new Player(3);
+		Player player4 = new Player(4);
+
+		player0.readyUp();
+		player1.readyUp();
+		player2.readyUp();
+		player3.readyUp();
+		player4.readyUp();
+
+		game.addPlayer(player0);
+		game.addPlayer(player1);
+		game.addPlayer(player2);
+		game.addPlayer(player3);
+		game.addPlayer(player4);
+
+		game.removePlayer(4);
+		game.removePlayer(3);
+		game.removePlayer(2);
+		game.removePlayer(1);
+		player0.win();
+
+		assertTrue("Player 1, Player 2, Player 3 and Player 4 left the game so, Player 0 wins should be true",
+				player0.win);
+	}
+
+	/*
+	 * Test the 5 Players of the game try to remove player0 to see it loose
+	 */
+	@Test
+	public void testLose() {
+		Game game = new Game();
+
+		Player player0 = new Player(0);
+		Player player1 = new Player(1);
+
+		player0.readyUp();
+		player1.readyUp();
+
+		game.addPlayer(player0);
+		game.addPlayer(player1);
+
+		game.removePlayer(1);
+
+		assertFalse("Player 1 left the game so Player 1 is loser", !player1.lose);
+		assertTrue("Player 1 left the game so Player 1 is loser", player1.lose);
+	}
+
+	/*
+	 * Test the does the card distributed get sorted
+	 */
+	@Test
+	public void testsorting() {
+		Card c1 = new Card();
+		c1.value = Card.Value.ACE;
+		c1.suite = Card.Suite.DIAMONDS;
+
+		Card c2 = new Card();
+		c2.value = Card.Value.TWO;
+		c2.suite = Card.Suite.DIAMONDS;
+
+		Card c3 = new Card();
+		c3.value = Card.Value.SIX;
+		c3.suite = Card.Suite.DIAMONDS;
+
+		Card c4 = new Card();
+		c4.value = Card.Value.KING;
+		c4.suite = Card.Suite.SPADES;
+
+		Card testingsortcard[] = { c2, c1, c4, c3 };
+		Card correctsortCard[] = { c1, c2, c3, c4 };
+
+		Hand.sortHand(testingsortcard);
+
+		for (int i = 0; i < 4; i++) {
+			if (testingsortcard[i] == correctsortCard[i])
+				assertTrue(true);
+		}
+	}
+
+	/*
+	 * Test the 2 Players of the game, does the card distributed
+	 */
+	@Test
+	public void testHand() {
+		Game game = new Game();
+
+		Player player0 = new Player(0);
+		Player player1 = new Player(1);
+
+		player0.readyUp();
+		player1.readyUp();
+
+		game.addPlayer(player0);
+		game.addPlayer(player1);
+
+		// the cards are distributed to all players
+		game.deal();
+
+		assertTrue(player0.Cards.length != 0 && player1.Cards.length != 0);
+	}
 }
